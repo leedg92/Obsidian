cafb5bb35443faa282169bd6fe2fd31313d947c4
leedg92@naver.com <leedg92@naver.com>
2024-03-22 18:27:32 +0900

    vault backup: 2024-03-22 18:27:32

--- crawlingTestChorme.py
+++ crawlingTestChorme.py
@@ -1,43 +1,102 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager 
-from selenium.common.exceptions import NoSuchElementException
-
-#pip install beautifulsoup4 lxml
-#pip install pandas openpyxl
-# pip install requests
-# pip install requests beautifulsoup4
-# pip install selenium
-
-
-# from openpyxl import Workbook
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+import re
+import cx_Oracle
 
 chrome_options = Options()
-chrome_options.add_argument("--disable-web-security")  # 웹 보안 비활성화
-chrome_options.add_argument("--ignore-certificate-errors")
-chrome_options.add_argument("--no-sandbox")  # 샌드박스 모드 비활성화
-#chrome_options.add_argument("--headless")  # 헤드리스
-chrome_options.add_argument("--disable-dev-shm-usage")  # /dev/shm 파티션 사용 비활성화
-chrome_options.add_argument("--disable-gpu")  # GPU 가속 비활성화, Linux에서 headless 모드의 성능 향상
+chrome_options.add_argument("--headless")
+chrome_options.add_argument('--blink-settings=imagesEnabled=false')  # 이미지 로딩 비활성화
+chrome_options.add_experimental_option("prefs", {
+    "profile.managed_default_content_settings.images": 2,  # 이미지 로딩 비활성화
+})
+
+caps = DesiredCapabilities.CHROME.copy()
+caps['pageLoadStrategy'] = 'eager'  # 'normal', 'eager', 'none'
+
 chrome_options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 
+# 로컬에 설치된 크롬 드라이버의 경로
+driver_path = "C:\\Users\\sli004\\Downloads\\chromedriver-win32\\chromedriver.exe"
+
 # WebDriver 서비스 시작
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options) 
+driver = webdriver.Chrome(service=Service(driver_path), options=chrome_options)
 
 # 웹페이지 접속
-url = 'http://shop.coupang.com/A01015774?platform=p&locale=ko_KR&source=brandstore_sdp_atf&pid=7809951769&viid=87075782691&ocid=17792877'
+url = 'https://www.pknu.ac.kr/main/163?pageIndex=1&bbsId=2&searchCondition=title&searchKeyword=&cd=10001'
 driver.get(url)
+time.sleep(1)
 
-time.sleep(500)  
+data_list = []
 
-# AJAX로 로드된 모든 정보가 포함된 페이지의 특정 요소의 텍스트를 추출
-name_elements = driver.find_elements(By.CSS_SELECTOR, ".product-wrap .name")
+try:
+    current_page = 0
+    #while True:
+    while current_page < 3:
+        current_page += 1
 
-for name_element in name_elements:
-    print(name_element.text)
+        posts = driver.find_elements(By.CSS_SELECTOR, "tr" + (":not(.noti)" if current_page > 1 else ""))
+        for post in posts:
+            try:
+                title_element = post.find_element(By.CLASS_NAME, "bdlTitle")
+                title_text = title_element.text
+                title_link = title_element.find_element(By.TAG_NAME, "a").get_attribute("href")
+                
+                # Extracting number from the link
+                no_match = re.search(r"&no=(\d+)", title_link)
+                no = no_match.group(1) if no_match else None
 
-#driver.quit()
+                regi_date = post.find_element(By.CLASS_NAME, "bdlDate").text
+                regi_name = post.find_element(By.CLASS_NAME, "bdlUser").text
+                
+                data_list.append((no, title_text, regi_date, regi_name))
+            except NoSuchElementException:
+                continue
+
+        # Moving to the next page
+        pagination_links = driver.find_elements(By.CSS_SELECTOR, "li.paginate a")
+        next_page_found = False
+        for link in pagination_links:
+            if link.text == str(current_page + 1):
+                next_page_found = True
+                link.click()
+                time.sleep(1)
+                break
+
+        if not next_page_found:
+            break
+
+except Exception as e:
+    print(f"An exception occurred: {e}")
+
+finally:
+    print(data_list)
+    print("data_list")
+    driver.quit()
+
+# Oracle DB 접속 정보 설정
+dsnStr = cx_Oracle.makedsn("YourDBHost", YourDBPort, sid="YourDBSID")
+connection = cx_Oracle.connect(user="YourUsername", password="YourPassword", dsn=dsnStr)
+
+cursor = connection.cursor()
+
+# 데이터 삽입 쿼리
+insert_query = """
+INSERT INTO your_table_name (no, title, regi_date, regi_name)
+VALUES (:1, :2, :3, :4)
+"""
+
+# 데이터를 데이터베이스에 삽입하는 루프
+for item in data_list:
+    cursor.execute(insert_query, item)
+
+# 커밋으로 변경사항 적용
+connection.commit()
+
+# 자원 정리
+cursor.close()
+connection.close()
---
+++ 부경대학교 AI기반 학생중심의 지능형 교육지원시스템 구축 프로젝트/CMS 관련 전달사항.md
@@ -0,0 +1,16 @@
+### 파일 구조도
+<sub>- 이하에서 말하는 [모듈명]이란 용어사전 혹은 네이밍 룰에 기반한 기능 구분명입니다. </sub>
+##### 1. \*.java
+	- [모듈명]Controller
+	- [모듈명]Service (abstract)
+	- [모듈명]ServiceImpl (implement)
+	- [모듈명]Mapper (Data Access Object)
+##### 2. \*.xml
+	- [모듈명]SQL_Oracle
+
+
+### Naming Rule
+<sub>- 각 메뉴에 대한 파일이 이미 다수 존재하지만 새로 만들때 참고하시면 됩니다.</sub>
+
+##### 1. 사용자화면 / 관리자화면 구분
+	-관리자 화면 
